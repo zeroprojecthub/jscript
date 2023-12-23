8f12bc69b97d457d4b40d5c81540a9db4a0d338c
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2023-12-23 05:43:30 +0000

    data type (primary) is introduce

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name="rahul"    // string
+let age=33          // number
+let city;           // undefined
+let temp=null       // null
+let isgendermale=true // boolean
+
+
+// bigint  => used for stock market value
+// boolean => true/false
+// symbol  => unique (specialy used in component identifier in react)
+
+
+console.log(typeof name);
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object (it's error or mistake in javascript i dont know)
