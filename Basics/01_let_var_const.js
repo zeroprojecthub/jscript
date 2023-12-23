9a16554dd902091feeb7ec07253a1e03329ab1fe
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2023-12-23 05:08:31 +0000

    "basic of variable"

---
+++ Basics/01_let_var_const.js
@@ -0,0 +1,10 @@
+// console.log("Hello World");
+
+const accountId =333334
+let accountName="karan"
+var accountcity="surat"
+accountmo=9898989
+let accountstate;
+
+// console.log(accountId);
+console.table([accountId,accountName,accountcity,accountmo,accountstate])
