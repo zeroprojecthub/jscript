c5895b5f921d577b999fd4bac1d67ee49c2e8778
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2023-12-23 07:34:13 +0000

    Operation on consversion is added

--- Basics/03_dataconversion.js
+++ Basics/03_dataconversion.js
@@ -19,7 +19,17 @@ numvalue=Number(num)
 num=33
 numvalue=String(num)
 
-console.log(num,typeof num);
-console.log(numvalue,typeof numvalue);
+// console.log(num,typeof num);
+// console.log(numvalue,typeof numvalue);
 
 
+// operation 
+
+// console.log(1+3);
+// console.log(1+"4");
+// console.log(1+"5"+"6");  // its depend on first type 
+// console.log("5"+6);     
+// console.log("5"+3+3);
+// console.log(false);
+console.log(true);
+console.log(true+1);
