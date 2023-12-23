329688b53c5824449eb33c83477b112bee3400e8
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2023-12-23 09:44:57 +0000

    data type and comparision operater

--- Basics/02_datatypes.js
+++ Basics/02_datatypes.js
@@ -13,3 +13,40 @@ let isgendermale=true // boolean
 console.log(typeof name);
 console.log(typeof undefined);  // undefined
 console.log(typeof null);       // object (it's error or mistake in javascript i dont know)
+
+// mainly two type of Data-type 
+//1.Primary 
+
+//String
+let fname="rahul raj"
+//Number
+let num=23232
+//BigInt
+let bnum=23232323n
+//Boolean
+let isit=false
+//null
+let tempofplant=null
+//undefined
+let onlydeclare;
+//Symbol
+let id1 = Symbol('123')
+let id2 = Symbol('123')
+/* 2.Reference Data Type
+// arrray , object , function
+
+// array
+let fruits = ["banana","mango","apple"]
+
+// object
+let student = {
+    name:"kishan",
+    age:33
+}
+// function
+
+let display = function(){
+
+ }
+
+*/
---
+++ Basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>3)
+// console.log(22>=11)
+// console.log(33>=33)
+
+// console.log("33">34)
+// console.log(55>"rr")
+
+// console.log(null==0);
+// console.log(null==1);
+
+// console.log(null==undefined);
+console.log(null>'1');
+
+// There is difference between == nad relation operater >,<,<=,>=
+
+
+
+
+
+
+
+
+
+
+
