c0ff54ab8567f823ea37450c1e391512af0deef6
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2023-12-25 03:48:11 +0000

    number and maths object

---
+++ Basics/06_numandmaths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(10000000)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+// console.log(balance.toPrecision(4));
+const another = 100000;
+
+// console.log(another.toLocaleString('en-IN'));
+// console.log(balance.toLocaleString('en-IN', { timeZone: 'UTC' }));
+
+// math
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(3.33));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
