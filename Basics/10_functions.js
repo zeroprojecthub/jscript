8fb2cfbdc350263ef7a37d180060e1f30bbbe405
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2023-12-26 07:14:57 +0000

    function creation

---
+++ Basics/10_functions.js
@@ -0,0 +1,72 @@
+
+function sayMyName(){
+    console.log("P");
+    console.log("D");
+}
+
+
+// sayMyName();
+
+function addTwoNumbers(number1, number2){
+
+    console.log(number1 + number2);
+}
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+// const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(name)
+{
+    if(!name)
+    {
+        console.log("Pleadse Enter user name");
+        return
+    }
+    return `${name} just loggin`;
+}
+
+// console.log(loginUserMessage("rahul"));
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+
+const user = {
+    username: "kavan",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+
+handleObject({
+    username: "sam",
+    price: 399
+});
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 500, 1000]));
