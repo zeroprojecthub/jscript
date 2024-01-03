5ccef9423f11eedcb8620e6b3c7aa3aeef44507a
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2024-01-03 04:12:07 +0000

    all promises

--- advance/promise.js
+++ advance/promise.js
@@ -23,12 +23,88 @@
 // })
 
 
-const promisethree = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        resolve({name:"karan",age:20});
-    },1000);
-})
+// const promisethree = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve({name:"karan",age:20});
+//     },1000);
+// })
 
-promisethree.then(function(data){
-    console.log(data);
-});
+// promisethree.then(function(data){
+//     console.log(data);
+// });
+
+
+
+// const promisefour = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         let error = false;
+//         if (!error) {
+//             resolve({ username: "karan", pasword: "12334" })
+//         }
+//         else {
+//             reject('Something  went wrong');
+//         }
+//     }, 1000)
+// })
+
+
+// promisefour.then((user) => {
+//     console.log(user);
+//     return user.username;
+// }).then((username) => {
+//     console.log(username);
+// }).catch(function (error) {
+//     console.log(error);
+// }).finally(() => {
+//     console.log('the promise is eiter resolve aur rejected');
+// })
+
+
+// const promisefive = new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         let error=false;
+//         if(!error){
+//             resolve({user:"kavan",pass:"1234"})
+//         }else{
+//             reject('ERRROR: JS WENT WRONG')
+//         }
+//     },1000);
+// })
+
+
+//  async function consumeprimsefive(){
+//     try{
+//         const response =  await promisefive;
+//         console.log(response);    
+//     }catch(error){
+//             console.log(error);
+//     }
+//    }
+
+// consumeprimsefive();
+
+// async function getAllUsers(){
+//     try{
+
+//         let response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         let data = await response.json();
+//         console.log(data);
+
+//     }catch(error){
+//             console.log('E::',error);
+//     }
+// }
+
+// getAllUsers();
+
+
+// fetch('https://api.github.com/users/zeroprojecthub')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => console.log(error))
+
+// promise.all
